fa5d66af7f63c4c6aaec0e748cbbf9ae11c946b8
andreaminolfi <andrea@minolfi.it>
2022-04-08 14:19:21 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  img =
+    'https://res.cloudinary.com/my-notes-demo/image/upload/v1627760417/academy/imac.png';
+  width = 100;
+
+  openImage(): void {
+    window.open(this.img, '_blank');
+  }
+
+  randomImage(): void {
+    this.img = 'https://source.unsplash.com/random?x=' + Math.random();
+  }
+
+  incWidth(): void {
+    if (this.width < 100) {
+      this.width++;
+    }
+  }
+
+  decWidth(): void {
+    if (this.width > 10) {
+      this.width--;
+    }
+  }
 }
